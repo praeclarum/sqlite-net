ce64b3e2149ca874783355c849d260cef5e88443
fstokke <frode.stokke@gmail.com>
2011-06-29 13:59:30 +0200

    All prepared SQL INSERT statements are now finalized before SQLite DB connection is closed to prevent SQLite3.Close() call from failing. Fixed problem that caused cached _insertSql statements to be used even if "extra" part of Insert() call was changed.

--- src/SQLite.cs
+++ src/SQLite.cs
@@ -673,6 +673,9 @@ namespace SQLite
 		{
 			if (_open && Handle != NullHandle) {
                 try {
+                    foreach (var sqlInsertCommand in _mappings.Values) {
+                        sqlInsertCommand.Dispose();
+                    }
                     var r = SQLite3.Close(Handle);
                     if (r != SQLite3.Result.OK) {
                         string msg = SQLite3.GetErrmsg(Handle);
@@ -735,7 +738,6 @@ namespace SQLite
 
 		Column _autoPk = null;
 		Column[] _insertColumns = null;
-		string _insertSql = null;
 
 		public TableMapping (Type type)
 		{
@@ -786,30 +788,46 @@ namespace SQLite
 			return exact;
 		}
 
-		public string InsertSql (string extra)
-		{
-			if (_insertSql == null) {
-				var cols = InsertColumns;
-				_insertSql = string.Format ("insert {3} into \"{0}\"({1}) values ({2})", TableName, string.Join (",", (from c in cols
-					select "\"" + c.Name + "\"").ToArray ()), string.Join (",", (from c in cols
-					select "?").ToArray ()), extra);
-			}
-			return _insertSql;
-		}
-
+		
 		PreparedSqlLiteInsertCommand _insertCommand;
 		string _insertCommandExtra = null;
 
-		public PreparedSqlLiteInsertCommand GetInsertCommand (SQLiteConnection conn, string extra)
-		{
-			if (_insertCommand == null || _insertCommandExtra != extra) {
-				var insertSql = InsertSql (extra);
-				_insertCommand = new PreparedSqlLiteInsertCommand (conn);
-				_insertCommand.CommandText = insertSql;
-				_insertCommandExtra = extra;
-			}
-			return _insertCommand;
-		}
+        public PreparedSqlLiteInsertCommand GetInsertCommand(SQLiteConnection conn, string extra)
+        {
+
+            if (_insertCommand == null) {
+                _insertCommand = CreateInsertCommand(conn, extra);
+                _insertCommandExtra = extra;
+            }
+            else if (_insertCommandExtra != extra) {
+                _insertCommand.Dispose();
+                _insertCommand = CreateInsertCommand(conn, extra);
+                _insertCommandExtra = extra;
+            }
+            return _insertCommand;
+        }
+
+        private PreparedSqlLiteInsertCommand CreateInsertCommand(SQLiteConnection conn, string extra)
+        {
+            var cols = InsertColumns;
+            var insertSql = string.Format ("insert {3} into \"{0}\"({1}) values ({2})", TableName, 
+                string.Join (",", (from c in cols
+                                   select "\"" + c.Name + "\"").ToArray ()), 
+                                   string.Join (",", (from c in cols
+                                                      select "?").ToArray ()), extra);
+                                                                                                                                                                   
+            var insertCommand = new PreparedSqlLiteInsertCommand(conn);
+            insertCommand.CommandText = insertSql;
+            return insertCommand;
+        }
+
+        protected internal void Dispose()
+        {
+            if (_insertCommand != null) {
+                _insertCommand.Dispose();
+                _insertCommand = null;
+            }
+        }
 
 		public abstract class Column
 		{
