617156031ed7d25bdcc24488b4a6d5afd9168740
Gonzalo Paniagua Javier <gonzalo@xamarin.com>
2012-03-21 02:07:25 -0400

    Fix the formatting

--- tests/UniqueTest.cs
+++ tests/UniqueTest.cs
@@ -9,8 +9,8 @@ namespace SQLite.Tests
 	public class UniqueIndexTest
 	{
 		public class TheOne {
-            [PrimaryKey, AutoIncrement]
-            public int ID { get; set; }
+			[PrimaryKey, AutoIncrement]
+			public int ID { get; set; }
 
 			[Unique (Name = "UX_Uno")]
 			public int Uno { get; set;}
@@ -29,48 +29,44 @@ namespace SQLite.Tests
 			public int Seis { get; set;}
 		}
 
-        public class IndexColumns {
-            public int seqno { get; set;} 
-            public int cid { get; set;} 
-            public string name { get; set; } 
-        }
+		public class IndexColumns {
+			public int seqno { get; set;} 
+			public int cid { get; set;} 
+			public string name { get; set; } 
+		}
 
-        public class IndexInfo {
-            public int seq { get; set;} 
-            public string name { get; set;} 
-            public bool unique { get; set;}
-        }
+		public class IndexInfo {
+			public int seq { get; set;} 
+			public string name { get; set;} 
+			public bool unique { get; set;}
+		}
 
 		[Test]
 		public void CreateUniqueIndexes ()
 		{
 			using (var db = new TestDb ()) {
-    			db.CreateTable<TheOne> ();
-                var indexes = db.Query<IndexInfo> ("PRAGMA INDEX_LIST (\"TheOne\")");
-                Assert.AreEqual (4, indexes.Count, "# of indexes");
-                CheckIndex (db, indexes, "UX_Uno", true, "Uno");
-                CheckIndex (db, indexes, "UX_Dos", true, "Dos", "Tres");
-                CheckIndex (db, indexes, "UX_Uno_bool", true, "Cuatro");
-                CheckIndex (db, indexes, "UX_Dos_bool", true, "Cinco", "Seis");
-            }
-            
+				db.CreateTable<TheOne> ();
+				var indexes = db.Query<IndexInfo> ("PRAGMA INDEX_LIST (\"TheOne\")");
+				Assert.AreEqual (4, indexes.Count, "# of indexes");
+				CheckIndex (db, indexes, "UX_Uno", true, "Uno");
+				CheckIndex (db, indexes, "UX_Dos", true, "Dos", "Tres");
+				CheckIndex (db, indexes, "UX_Uno_bool", true, "Cuatro");
+				CheckIndex (db, indexes, "UX_Dos_bool", true, "Cinco", "Seis");
+			}
 		}
 
-        static void CheckIndex (TestDb db, List<IndexInfo> indexes, string iname, bool unique, params string [] columns)
-        {
-            if (columns == null)
-                throw new Exception ("Don't!");
-            var idx = indexes.SingleOrDefault (i => i.name == iname);
-            foreach (var p in indexes) {
-                Console.Error.WriteLine (p);
-            }
-            Assert.NotNull (idx, String.Format ("Index {0} not found", iname));
-            Assert.AreEqual (idx.unique, unique, String.Format ("Index {0} unique expected {1} but got {2}", iname, unique, idx.unique));
-            var idx_columns = db.Query<IndexColumns> (String.Format ("PRAGMA INDEX_INFO (\"{0}\")", iname));
-            Assert.AreEqual (columns.Length, idx_columns.Count, String.Format ("# of columns: expected {0}, got {1}", columns.Length, idx_columns.Count));
-            foreach (var col in columns) {
-                Assert.NotNull (idx_columns.SingleOrDefault (c => c.name == col), String.Format ("Column {0} not in index {1}", col, idx.name));
-            }
-        }
+		static void CheckIndex (TestDb db, List<IndexInfo> indexes, string iname, bool unique, params string [] columns)
+		{
+			if (columns == null)
+				throw new Exception ("Don't!");
+			var idx = indexes.SingleOrDefault (i => i.name == iname);
+			Assert.NotNull (idx, String.Format ("Index {0} not found", iname));
+			Assert.AreEqual (idx.unique, unique, String.Format ("Index {0} unique expected {1} but got {2}", iname, unique, idx.unique));
+			var idx_columns = db.Query<IndexColumns> (String.Format ("PRAGMA INDEX_INFO (\"{0}\")", iname));
+			Assert.AreEqual (columns.Length, idx_columns.Count, String.Format ("# of columns: expected {0}, got {1}", columns.Length, idx_columns.Count));
+			foreach (var col in columns) {
+				Assert.NotNull (idx_columns.SingleOrDefault (c => c.name == col), String.Format ("Column {0} not in index {1}", col, idx.name));
+			}
+		}
 	}
 }
