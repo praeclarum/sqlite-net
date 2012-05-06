ba88de7123321574ea39c8f8cf9348d5ab58a147
David Clarke <davidclarke@zebracrossing.co.nz>
2012-05-06 12:20:19 +1200

    Added Sguid short guid type for use as primary key column

--- src/SQLite.cs
+++ src/SQLite.cs
@@ -46,6 +46,274 @@ using Sqlite3Statement = System.IntPtr;
 
 namespace SQLite
 {
+	#region Sguid
+	/// <summary>
+	/// Represents a globally unique identifier (GUID) with a 
+	/// shorter string value. Sguid.
+	/// </summary>
+	/// <seealso cref="http://www.singular.co.nz/blog/archive/2007/12/20/shortguid-a-shorter-and-url-friendly-guid-in-c-sharp.aspx"/>
+	public struct Sguid
+	{
+		#region Static
+
+		/// <summary>
+		/// A read-only instance of the SGuid class whose value 
+		/// is guaranteed to be all zeroes. 
+		/// </summary>
+		public static readonly Sguid Empty = new Sguid(Guid.Empty);
+
+		#endregion
+
+		#region Fields
+
+		Guid _guid;
+		string _value;
+
+		#endregion
+
+		#region Contructors
+
+		/// <summary>
+		/// Creates a Sguid from a base64 encoded string
+		/// </summary>
+		/// <param name="value">The encoded guid as a 
+		/// base64 string</param>
+		public Sguid(string value)
+		{
+			_value = value;
+			_guid = Decode(value);
+		}
+
+		/// <summary>
+		/// Creates a Sguid from a Guid
+		/// </summary>
+		/// <param name="guid">The Guid to encode</param>
+		public Sguid(Guid guid)
+		{
+			_value = Encode(guid);
+			_guid = guid;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets/sets the underlying Guid
+		/// </summary>
+		public Guid Guid
+		{
+			get { return _guid; }
+			set
+			{
+				if (value != _guid)
+				{
+					_guid = value;
+					_value = Encode(value);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets/sets the underlying base64 encoded string
+		/// </summary>
+		public string Value
+		{
+			get { return _value; }
+			set
+			{
+				if (value != _value)
+				{
+					_value = value;
+					_guid = Decode(value);
+				}
+			}
+		}
+
+		#endregion
+
+		#region ToString
+
+		/// <summary>
+		/// Returns the base64 encoded guid as a string
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return _value;
+		}
+
+		#endregion
+
+		#region Equals
+
+		/// <summary>
+		/// Returns a value indicating whether this instance and a 
+		/// specified Object represent the same type and value.
+		/// </summary>
+		/// <param name="obj">The object to compare</param>
+		/// <returns></returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is Sguid)
+				return _guid.Equals(((Sguid)obj)._guid);
+			if (obj is Guid)
+				return _guid.Equals((Guid)obj);
+			if (obj is string)
+				return _guid.Equals(((Sguid)obj)._guid);
+			return false;
+		}
+
+		#endregion
+
+		#region GetHashCode
+
+		/// <summary>
+		/// Returns the HashCode for underlying Guid.
+		/// </summary>
+		/// <returns></returns>
+		public override int GetHashCode()
+		{
+			return _guid.GetHashCode();
+		}
+
+		#endregion
+
+		#region NewGuid
+
+		/// <summary>
+		/// Initialises a new instance of the Sguid class
+		/// </summary>
+		/// <returns></returns>
+		public static Sguid NewSguid
+		{
+			get {
+				return new Sguid(Guid.NewGuid());
+			}
+		}
+
+		#endregion
+
+		#region Encode
+
+		/// <summary>
+		/// Creates a new instance of a Guid using the string value, 
+		/// then returns the base64 encoded version of the Guid.
+		/// </summary>
+		/// <param name="value">An actual Guid string (i.e. not a Sguid)</param>
+		/// <returns></returns>
+		public static string Encode(string value)
+		{
+			Guid guid = new Guid(value);
+			return Encode(guid);
+		}
+
+		/// <summary>
+		/// Encodes the given Guid as a base64 string that is 22 
+		/// characters long.
+		/// </summary>
+		/// <param name="guid">The Guid to encode</param>
+		/// <returns></returns>
+		public static string Encode(Guid guid)
+		{
+			string encoded = Convert.ToBase64String(guid.ToByteArray());
+			encoded = encoded
+				.Replace("/", "_")
+				.Replace("+", "-");
+			return encoded.Substring(0, 22);
+		}
+
+		#endregion
+
+		#region Decode
+
+		/// <summary>
+		/// Decodes the given base64 string
+		/// </summary>
+		/// <param name="value">The base64 encoded string of a Guid</param>
+		/// <returns>A new Guid</returns>
+		public static Guid Decode(string value)
+		{
+			value = value
+				.Replace("_", "/")
+				.Replace("-", "+");
+			byte[] buffer = Convert.FromBase64String(value + "==");
+			return new Guid(buffer);
+		}
+
+		#endregion
+
+		#region Operators
+
+		/// <summary>
+		/// Determines if both Sguids have the same underlying 
+		/// Guid value.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		public static bool operator ==(Sguid x, Sguid y)
+		{
+			if ((object)x == null) return (object)y == null;
+			return x._guid == y._guid;
+		}
+
+		/// <summary>
+		/// Determines if both Sguids do not have the 
+		/// same underlying Guid value.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		public static bool operator !=(Sguid x, Sguid y)
+		{
+			return !(x == y);
+		}
+
+		/// <summary>
+		/// Implicitly converts the Sguid to it's string equivilent
+		/// </summary>
+		/// <param name="shortGuid"></param>
+		/// <returns></returns>
+		public static implicit operator string(Sguid shortGuid)
+		{
+			return shortGuid._value;
+		}
+
+		/// <summary>
+		/// Implicitly converts the Sguid to it's Guid equivilent
+		/// </summary>
+		/// <param name="shortGuid"></param>
+		/// <returns></returns>
+		public static implicit operator Guid(Sguid shortGuid)
+		{
+			return shortGuid._guid;
+		}
+
+		/// <summary>
+		/// Implicitly converts the string to a Sguid
+		/// </summary>
+		/// <param name="shortGuid"></param>
+		/// <returns></returns>
+		public static implicit operator Sguid(string shortGuid)
+		{
+			return new Sguid(shortGuid);
+		}
+
+		/// <summary>
+		/// Implicitly converts the Guid to a Sguid 
+		/// </summary>
+		/// <param name="guid"></param>
+		/// <returns></returns>
+		public static implicit operator Sguid(Guid guid)
+		{
+			return new Sguid(guid);
+		}
+
+		#endregion
+	}
+	#endregion
+	
 	public class SQLiteException : System.Exception
 	{
 		public SQLite3.Result Result { get; private set; }
@@ -1072,6 +1340,8 @@ namespace SQLite
 				return "varchar(" + len + ")";
 			} else if (clrType == typeof(DateTime)) {
 				return "datetime";
+			} else if (clrType == typeof(Sguid)) {
+				return "sguid";
 #if !NETFX_CORE
 			} else if (clrType.IsEnum) {
 #else
@@ -1334,6 +1604,8 @@ namespace SQLite
 					SQLite3.BindDouble (stmt, index, Convert.ToDouble (value));
 				} else if (value is DateTime) {
 					SQLite3.BindText (stmt, index, ((DateTime)value).ToString ("yyyy-MM-dd HH:mm:ss"), -1, NegativePointer);
+				} else if (value is Sguid) {
+					SQLite3.BindText (stmt, index, ((Sguid)value).ToString (), -1, NegativePointer);
 #if !NETFX_CORE
 				} else if (value.GetType().IsEnum) {
 #else
@@ -1375,6 +1647,9 @@ namespace SQLite
 				} else if (clrType == typeof(DateTime)) {
 					var text = SQLite3.ColumnString (stmt, index);
 					return DateTime.Parse (text);
+				} else if (clrType == typeof(Sguid)) {
+					var text = SQLite3.ColumnString (stmt, index);
+					return new Sguid(text);
 #if !NETFX_CORE
 				} else if (clrType.IsEnum) {
 #else
