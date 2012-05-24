640d9a55b9102213c8f58045d01da81721e01f87
Frank A. Krueger <fak@praeclarum.org>
2012-05-24 11:14:59 -0700

    Fixed test bug on non-Metro

--- tests/BooleanTest.cs
+++ tests/BooleanTest.cs
@@ -5,12 +5,13 @@ using System.Linq;
 using System.Text;
 using SQLite;
 
+using System.Diagnostics;
+
 #if NETFX_CORE
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SetUp = Microsoft.VisualStudio.TestTools.UnitTesting.TestInitializeAttribute;
 using TestFixture = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
 using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
-using System.Diagnostics;
 #else
 using NUnit.Framework;
 #endif
