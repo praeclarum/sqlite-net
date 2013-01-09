c7185a2066424a0b77ce82acf033e6208c06e64d
Vegard Andreas Larsen <veg@rd.no>
2013-01-09 13:42:49 +0100

    Adds support for Equals() in Where expressions.

--- src/SQLite.cs
+++ src/SQLite.cs
@@ -2359,6 +2359,9 @@ namespace SQLite
 				else if (call.Method.Name == "EndsWith" && args.Length == 1) {
 					sqlCall = "(" + obj.CommandText + " like ('%' || " + args [0].CommandText + "))";
 				}
+				else if (call.Method.Name == "Equals" && args.Length == 1) {
+					sqlCall = "(" + obj.CommandText + " = (" + args[0].CommandText + "))";
+				}
 				else {
 					sqlCall = call.Method.Name.ToLower () + "(" + string.Join (",", args.Select (a => a.CommandText).ToArray ()) + ")";
 				}
