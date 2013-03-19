841ac3ba3bba190093147b714be16a8e2e765e18
Peter Huene <peterhuene@gmail.com>
2013-03-19 16:30:17 -0700

    Adding EnableLoadExtension so that csharp-sqlite/sqlite-net-wp8 can build.

--- src/SQLite.cs
+++ src/SQLite.cs
@@ -3046,6 +3046,11 @@ namespace SQLite
 		{
 			return ColumnBlob(stmt, index);
 		}
+
+		public static Result EnableLoadExtension(Sqlite3DatabaseHandle db, int onoff)
+		{
+			return (Result)Sqlite3.sqlite3_enable_load_extension(db, onoff);
+		}
 #endif
 
 		public enum ColType : int
