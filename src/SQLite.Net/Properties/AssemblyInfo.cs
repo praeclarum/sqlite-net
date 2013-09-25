8dcb4841e2f2b7935b384f8bcfd8466774300a71
Øystein Krog <oystein.krog@gmail.com>
2013-09-25 21:01:16 +0200

    Add nunit nuget to the tests project

--- src/SQLite.Net/Properties/AssemblyInfo.cs
+++ src/SQLite.Net/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("SQLite.Tests")]
+[assembly: InternalsVisibleTo("SQLite.Net.Tests")]
