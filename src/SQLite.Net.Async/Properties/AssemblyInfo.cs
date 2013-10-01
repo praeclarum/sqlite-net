2357bb2aab927eb439e8714b8321a6f4265e1732
Øystein Krog <oystein.krog@gmail.com>
2013-10-01 15:49:06 +0200

    Convert SQLite.Net.Async project to PCL

--- src/SQLite.Net.Async/Properties/AssemblyInfo.cs
+++ src/SQLite.Net.Async/Properties/AssemblyInfo.cs
@@ -14,16 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("f67b7656-4071-4159-af1d-7f6770a26a46")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
