fbb1a2457a72712a1e97df789737864969a5f7dd
Øystein Krog <oystein.krog@gmail.com>
2013-10-04 16:17:16 +0200

    Add SQLite.Net.Platform.WindowsPhone71

---
+++ src/SQLite.Net.Platform.WindowsPhone71/VolatileServiceWP71.cs
@@ -0,0 +1,14 @@
+﻿using SQLite.Net.Interop;
+
+namespace SQLite.Net.Platform.WindowsPhone71
+{
+    public class VolatileServiceWP71 : IVolatileService
+    {
+        public void Write(ref int transactionDepth, int depth)
+        {
+            // :/
+            // TODO: this is not good!
+            transactionDepth = depth;
+        }
+    }
+}
