e9820549ce3821b204f4cbb852f072539b3ff7e8
Øystein Krog <oystein.krog@gmail.com>
2013-10-13 14:53:11 +0200

    Improve nuget package tooling and update/add nuspec files

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,1 +1,6 @@
-nuget pack sqlite-net.nuspec -o .\
+@mkdir output
+..\.nuget\nuget pack SQLite.Net.nuspec -o output
+..\.nuget\nuget pack SQLite.Net.Async.nuspec -o output
+..\.nuget\nuget pack SQLite.Net.Platform.Win32.nuspec -o output
+..\.nuget\nuget pack SQLite.Net.Platform.XamarinAndroid.nuspec -o output
+..\.nuget\nuget pack SQLite.Net.Platform.XamarinIOS.nuspec -o output
---
+++ nuget/prepare.bat
@@ -0,0 +1,14 @@
+mkdir SQLite.Net
+copy ..\src\SQLite.Net\bin\Release\SQLite.Net.dll SQLite.Net
+
+mkdir SQLite.Net.Async
+copy ..\src\SQLite.Net.Async\bin\Release\SQLite.Net.Async.dll SQLite.Net.Async
+
+mkdir SQLite.Net.Platform.Win32
+copy ..\src\SQLite.Net.Platform.Win32\bin\Release\SQLite.Net.Platform.Win32.dll SQLite.Net.Platform.Win32
+
+mkdir SQLite.Net.Platform.XamarinAndroid
+copy ..\src\SQLite.Net.Platform.XamarinAndroid\bin\Release\SQLite.Net.Platform.XamarinAndroid.dll SQLite.Net.Platform.XamarinAndroid
+
+mkdir SQLite.Net.Platform.XamarinIOS
+copy ..\src\SQLite.Net.Platform.XamarinIOS\bin\iPhone\Release\SQLite.Net.Platform.XamarinIOS.dll SQLite.Net.Platform.XamarinIOS
