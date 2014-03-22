a6795f72df9f166af79390f08ca443e4e7a7ff2b
Øystein Krog <oystein.krog@gmail.com>
2014-03-22 23:04:29 +0100

    Add new platform-specific test projects, uses file linking to shared test files :/

---
+++ tests/SQLite.Net.Tests.WindowsPhone8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SQLite.Net.Tests.WindowsPhone8.Resources;
+
+namespace SQLite.Net.Tests.WindowsPhone8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
