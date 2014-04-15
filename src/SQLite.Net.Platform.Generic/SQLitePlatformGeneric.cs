4b55dcec2dff0300194cbad6eb5d20e850c79669
James Ottaway <git@james.ottaway.io>
2014-04-15 15:20:17 +1000

    Clone the Xamarin platforms to make a generic one

---
+++ src/SQLite.Net.Platform.Generic/SQLitePlatformGeneric.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.Net.Interop;
+
+namespace SQLite.Net.Platform.Generic
+{
+    public class SQLitePlatformGeneric : ISQLitePlatform
+    {
+        public SQLitePlatformGeneric()
+        {
+            SQLiteApi = new SQLiteApiGeneric();
+            StopwatchFactory = new StopwatchFactoryGeneric();
+            ReflectionService = new ReflectionServiceGeneric();
+            VolatileService = new VolatileServiceGeneric();
+        }
+
+        public ISQLiteApi SQLiteApi { get; private set; }
+        public IStopwatchFactory StopwatchFactory { get; private set; }
+        public IReflectionService ReflectionService { get; private set; }
+        public IVolatileService VolatileService { get; private set; }
+    }
+}
