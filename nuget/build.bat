a0b0a45aa305a7d6a1dfeb0a236fdbfd6e2b0b83
James Ottaway <git@james.ottaway.io>
2014-05-13 19:19:25 +1000

    Configure SQLite.Net.Platform.Generic for NuGet

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,6 +1,7 @@
 @mkdir output
 ..\.nuget\nuget pack SQLite.Net.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.Async.nuspec -o output
+..\.nuget\nuget pack SQLite.Net.Platform.Generic.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.Platform.WindowsPhone8.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.Platform.Win32.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.Platform.WinRT.nuspec -o output
