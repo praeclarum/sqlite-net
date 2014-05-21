6c93ec5ad7b1fe26e7b00492b511781f88864e01
Øystein Krog <oystein.krog@gmail.com>
2014-05-21 07:53:26 +0200

    Bump version to 2.3.0.0 and update release notes in nugets

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -11,5 +11,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
