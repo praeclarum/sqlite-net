b71e20fe8d006353fa73852388edb94c77cf4827
Derek Beattie <derek.beattie@gmail.com>
2014-05-24 13:19:49 -0500

    Add WindowsPhoneApp81 project

---
+++ src/SQLite.Net.Platform.WindowsPhoneApp81/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SQLite.Net.Platform.WindowsPhoneApp81")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Øystein Krog")]
+[assembly: AssemblyProduct("SQLite.Net.Platform.WinRT")]
+[assembly: AssemblyCopyright("Copyright © Øystein Krog")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
