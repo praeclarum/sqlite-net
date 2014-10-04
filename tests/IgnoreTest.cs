9ee59d23f91627b514a2afb95dfd95a41e37cee7
Øystein Krog <oystein.krog@gmail.com>
2014-10-04 11:28:34 +0200

    Add test for Ignore attribute

---
+++ tests/IgnoreTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SQLite.Net.Attributes;
+
+#if __WIN32__
+using SQLitePlatformTest = SQLite.Net.Platform.Win32.SQLitePlatformWin32;
+#elif WINDOWS_PHONE
+using SQLitePlatformTest = SQLite.Net.Platform.WindowsPhone8.SQLitePlatformWP8;
+#elif __WINRT__
+using SQLitePlatformTest = SQLite.Net.Platform.WinRT.SQLitePlatformWinRT;
+#elif __IOS__
+using SQLitePlatformTest = SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS;
+#elif __ANDROID__
+using SQLitePlatformTest = SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid;
+#else
+using SQLitePlatformTest = SQLite.Net.Platform.Generic.SQLitePlatformGeneric;
+#endif
+
+
+namespace SQLite.Net.Tests
+{
+    [TestFixture]
+    public class IgnoredTest
+    {
+        public class DummyClass
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+
+            public string Foo { get; set; }
+            public string Bar { get; set; }
+
+            [Attributes.Ignore]
+            public List<string> Ignored { get; set; }
+        }
+
+        [Test]
+        public void NullableFloat()
+        {
+            var db = new SQLiteConnection(new SQLitePlatformTest(), TestPath.GetTempFileName());
+            // if the Ignored property is not ignore this will cause an exception
+            db.CreateTable<DummyClass>();
+        }
+    }
+}
