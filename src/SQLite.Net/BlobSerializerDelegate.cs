4533449ba6131a36c4263bc9529c5e0c21471ba9
Øystein Krog <oystein.krog@gmail.com>
2015-01-10 12:05:37 +0100

    core/BlobSerializer: Rename fields name pattern to match rest of code

--- src/SQLite.Net/BlobSerializerDelegate.cs
+++ src/SQLite.Net/BlobSerializerDelegate.cs
@@ -5,40 +5,41 @@ namespace SQLite.Net
     public class BlobSerializerDelegate : IBlobSerializer
     {
         public delegate byte[] SerializeDelegate(object obj);
+
         public delegate bool CanSerializeDelegate(Type type);
+
         public delegate object DeserializeDelegate(byte[] data, Type type);
 
-        private readonly SerializeDelegate serializeDelegate;
-        private readonly DeserializeDelegate deserializeDelegate;
-        private readonly CanSerializeDelegate canDeserializeDelegate;
+        private readonly SerializeDelegate _serializeDelegate;
+        private readonly DeserializeDelegate _deserializeDelegate;
+        private readonly CanSerializeDelegate _canDeserializeDelegate;
 
-        public BlobSerializerDelegate (SerializeDelegate serializeDelegate, 
+        public BlobSerializerDelegate(SerializeDelegate serializeDelegate,
             DeserializeDelegate deserializeDelegate,
             CanSerializeDelegate canDeserializeDelegate)
         {
-            this.serializeDelegate = serializeDelegate;
-            this.deserializeDelegate = deserializeDelegate;
-            this.canDeserializeDelegate = canDeserializeDelegate;
+            _serializeDelegate = serializeDelegate;
+            _deserializeDelegate = deserializeDelegate;
+            _canDeserializeDelegate = canDeserializeDelegate;
         }
 
         #region IBlobSerializer implementation
 
         public byte[] Serialize<T>(T obj)
         {
-            return this.serializeDelegate (obj);
+            return _serializeDelegate(obj);
         }
 
         public object Deserialize(byte[] data, Type type)
         {
-            return this.deserializeDelegate (data, type);
+            return _deserializeDelegate(data, type);
         }
 
         public bool CanDeserialize(Type type)
         {
-            return this.canDeserializeDelegate (type);
+            return _canDeserializeDelegate(type);
         }
 
         #endregion
     }
 }
-
