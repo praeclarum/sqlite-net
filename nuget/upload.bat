cbe05f67881eb74f1d3ef8d1c53a26fff84b2deb
Øystein Krog <oystein.krog@gmail.com>
2015-02-01 15:08:23 +0100

    packaging: Bump version to 2.5.1

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,8 +1,8 @@
-..\.nuget\nuget push output\SQLite.Net-PCL.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Async-PCL.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Platform.Generic.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Platform.WindowsPhone8.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Platform.Win32.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Platform.WinRT.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Platform.XamarinAndroid.2.5.nupkg
-..\.nuget\nuget push output\SQLite.Net.Platform.XamarinIOS.2.5.nupkg
+..\.nuget\nuget push output\SQLite.Net-PCL.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Async-PCL.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Platform.Generic.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Platform.WindowsPhone8.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Platform.Win32.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Platform.WinRT.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Platform.XamarinAndroid.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Platform.XamarinIOS.2.5.1.nupkg
