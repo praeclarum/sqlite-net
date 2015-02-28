82c6db7c8613d554d18f421c27be9af34a621a7e
Deniel Aloïs <alois.deniel@gmail.com>
2015-02-28 14:04:04 +0100

    Merged all nuspec files to have only one NuGet package.

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,9 +1,3 @@
 @mkdir output
 ..\.nuget\nuget pack SQLite.Net.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.Async.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Platform.Generic.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Platform.WindowsPhone8.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Platform.Win32.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Platform.WinRT.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Platform.XamarinAndroid.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Platform.XamarinIOS.nuspec -o output
