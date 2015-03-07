1c2b927de036ce5700d5499f1b626db21f62780e
Øystein Krog <oystein.krog@gmail.com>
2015-03-07 10:25:09 +0100

    tests: Fix test projects, use wildcard include for test files

--- tests/ContainsTest.cs
+++ tests/ContainsTest.cs
@@ -8,6 +8,8 @@ using SQLite.Net.Interop;
 using SQLitePlatformTest = SQLite.Net.Platform.Win32.SQLitePlatformWin32;
 #elif WINDOWS_PHONE
 using SQLitePlatformTest = SQLite.Net.Platform.WindowsPhone8.SQLitePlatformWP8;
+#elif __WINRT__
+using SQLitePlatformTest = SQLite.Net.Platform.WinRT.SQLitePlatformWinRT;
 #elif __IOS__
 using SQLitePlatformTest = SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS;
 #elif __ANDROID__
