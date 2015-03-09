865f686af15755d20c98dab5304fe47a7bcaa070
Øystein Krog <oystein.krog@gmail.com>
2015-03-09 22:15:59 +0100

    tests/TestDb: Fix indenting

--- tests/TestDb.cs
+++ tests/TestDb.cs
@@ -16,131 +16,123 @@ using SQLitePlatformTest = SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndr
 using SQLitePlatformTest = SQLite.Net.Platform.Generic.SQLitePlatformGeneric;
 #endif
 
-
 namespace SQLite.Net.Tests
 {
-	[Table("Product")]
-	public interface IProduct
-	{
-		[AutoIncrement, PrimaryKey]
-		int Id { get; set; }
-		string Name { get; set; }
-		decimal Price { get; set; }
-		uint TotalSales { get; set; }
-	}
+    [Table("Product")]
+    public interface IProduct
+    {
+        [AutoIncrement, PrimaryKey]
+        int Id { get; set; }
 
-	public class Product : IProduct
-	{
-		[AutoIncrement, PrimaryKey]
-		public int Id { get; set; }
+        string Name { get; set; }
+        decimal Price { get; set; }
+        uint TotalSales { get; set; }
+    }
 
-		public string Name { get; set; }
-		public decimal Price { get; set; }
+    public class Product : IProduct
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
 
-		public uint TotalSales { get; set; }
-	}
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public uint TotalSales { get; set; }
+    }
 
-	[Table("Order")]
-	public interface IOrder
-	{
-		[AutoIncrement, PrimaryKey]
-		int Id { get; set; }
+    [Table("Order")]
+    public interface IOrder
+    {
+        [AutoIncrement, PrimaryKey]
+        int Id { get; set; }
 
-		DateTime PlacedTime { get; set; }
-	}
+        DateTime PlacedTime { get; set; }
+    }
 
-	public class Order : IOrder
-	{
-		[AutoIncrement, PrimaryKey]
-		public int Id { get; set; }
+    public class Order : IOrder
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
 
-		public DateTime PlacedTime { get; set; }
-	}
+        public DateTime PlacedTime { get; set; }
+    }
 
-	[Table("OrderHistory")]
-	public interface IOrderHistory
-	{
-		[AutoIncrement, PrimaryKey]
-		int Id { get; set; }
+    [Table("OrderHistory")]
+    public interface IOrderHistory
+    {
+        [AutoIncrement, PrimaryKey]
+        int Id { get; set; }
 
-		int OrderId { get; set; }
+        int OrderId { get; set; }
+        DateTime Time { get; set; }
+        string Comment { get; set; }
+    }
 
-		DateTime Time { get; set; }
+    public class OrderHistory : IOrderHistory
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
 
-		string Comment { get; set; }
-	}
+        public int OrderId { get; set; }
+        public DateTime Time { get; set; }
+        public string Comment { get; set; }
+    }
 
-	public class OrderHistory : IOrderHistory
-	{
-		[AutoIncrement, PrimaryKey]
-		public int Id { get; set; }
+    [Table("OrderLine")]
+    public interface IOrderLine
+    {
+        [AutoIncrement, PrimaryKey]
+        int Id { get; set; }
 
-		public int OrderId { get; set; }
-		public DateTime Time { get; set; }
-		public string Comment { get; set; }
-	}
+        [Indexed("IX_OrderProduct", 1)]
+        int OrderId { get; set; }
 
-	[Table("OrderLine")]
-	public interface IOrderLine
-	{
-		[AutoIncrement, PrimaryKey]
-		int Id { get; set; }
+        [Indexed("IX_OrderProduct", 2)]
+        int ProductId { get; set; }
 
-		[Indexed("IX_OrderProduct", 1)]
-		int OrderId { get; set; }
+        int Quantity { get; set; }
+        decimal UnitPrice { get; set; }
+        OrderLineStatus Status { get; set; }
+    }
 
-		[Indexed("IX_OrderProduct", 2)]
-		int ProductId { get; set; }
+    public class OrderLine : IOrderLine
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
 
-		int Quantity { get; set; }
+        [Indexed("IX_OrderProduct", 1)]
+        public int OrderId { get; set; }
 
-		decimal UnitPrice { get; set; }
+        [Indexed("IX_OrderProduct", 2)]
+        public int ProductId { get; set; }
 
-		OrderLineStatus Status { get; set; }
-	}
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public OrderLineStatus Status { get; set; }
+    }
 
-	public class OrderLine : IOrderLine
-	{
-		[AutoIncrement, PrimaryKey]
-		public int Id { get; set; }
+    public enum OrderLineStatus
+    {
+        Placed = 1,
+        Shipped = 100
+    }
 
-		[Indexed("IX_OrderProduct", 1)]
-		public int OrderId { get; set; }
+    public class TestDb : SQLiteConnection
+    {
+        public TestDb(bool storeDateTimeAsTicks = false, IContractResolver resolver = null)
+            : base(
+                new SQLitePlatformTest(), TestPath.GetTempFileName(), storeDateTimeAsTicks, null,
+                extraTypeMappings: null,
+                resolver: resolver)
+        {
+            TraceListener = DebugTraceListener.Instance;
+        }
+    }
 
-		[Indexed("IX_OrderProduct", 2)]
-		public int ProductId { get; set; }
-
-		public int Quantity { get; set; }
-		public decimal UnitPrice { get; set; }
-		public OrderLineStatus Status { get; set; }
-	}
-
-	public enum OrderLineStatus
-	{
-		Placed = 1,
-		Shipped = 100
-	}
-
-	public class TestDb : SQLiteConnection
-	{
-		public TestDb(bool storeDateTimeAsTicks = false, IContractResolver resolver = null)
-			: base(
-				new SQLitePlatformTest(),
-				databasePath: TestPath.GetTempFileName(),
-				storeDateTimeAsTicks: storeDateTimeAsTicks,
-				serializer: null,
-				extraTypeMappings: null,
-				resolver: resolver)
-		{
-			TraceListener = DebugTraceListener.Instance;
-		}
-	}
-
-	public class TestPath
-	{
-		public static string GetTempFileName()
-		{
-			return Path.GetTempFileName();
-		}
-	}
+    public class TestPath
+    {
+        public static string GetTempFileName()
+        {
+            return Path.GetTempFileName();
+        }
+    }
 }
