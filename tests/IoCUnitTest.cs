778c30dff4e1b32783807424672c6c9756135524
Øystein Krog <oystein.krog@gmail.com>
2015-03-09 22:05:14 +0100

    tests/IoC: Rewrite to remove TinyIoC
    TinyIoC is not compatible with all platforms

--- tests/IoCUnitTest.cs
+++ tests/IoCUnitTest.cs
@@ -1,87 +1,126 @@
 ﻿using System;
-using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
 
 namespace SQLite.Net.Tests
 {
-	[TestFixture]
-	public class IoCUnitTests
-	{
-		private TinyIoC.TinyIoCContainer _container;
+    [TestFixture]
+    public class IoCUnitTests
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            _container = new SimpleIoC();
 
-		[SetUp]
-		public void Initialize()
-		{
-			_container = new TinyIoC.TinyIoCContainer();
+            _container.Register<IProduct, Product>();
+            _container.Register<IOrder, Order>();
+            _container.Register<IOrderHistory, OrderHistory>();
+            _container.Register<IOrderLine, OrderLine>();
+        }
 
-			_container.Register<IProduct, Product>();
-			_container.Register<IOrder, Order>();
-			_container.Register<IOrderHistory, OrderHistory>();
-			_container.Register<IOrderLine, OrderLine>();			
-		}
+        private SimpleIoC _container;
 
-		private static void VerifyCreations(TestDb db)
-		{
-			var orderLine = db.GetMapping(typeof(OrderLine));
-			Assert.AreEqual(6, orderLine.Columns.Length);
+        private static void VerifyCreations(TestDb db)
+        {
+            var orderLine = db.GetMapping(typeof (OrderLine));
+            Assert.AreEqual(6, orderLine.Columns.Length);
 
-			var l = new OrderLine
-			{
-				Status = OrderLineStatus.Shipped
-			};
-			db.Insert(l);
-			OrderLine lo = db.Table<OrderLine>().First(x => x.Status == OrderLineStatus.Shipped);
-			Assert.AreEqual(lo.Id, l.Id);
-		}
+            var l = new OrderLine
+            {
+                Status = OrderLineStatus.Shipped
+            };
+            db.Insert(l);
+            var lo = db.Table<OrderLine>().First(x => x.Status == OrderLineStatus.Shipped);
+            Assert.AreEqual(lo.Id, l.Id);
+        }
 
-		[Test]
-		public void CreateThemByInterface()
-		{
-			var db = new TestDb();
+        private class SimpleIoC
+        {
+            private readonly Dictionary<Type, Type> _typeMap = new Dictionary<Type, Type>();
 
-			db.CreateTable<IProduct>();
-			db.CreateTable<IOrder>();
-			db.CreateTable<IOrderLine>();
-			db.CreateTable<IOrderHistory>();
+            public void Register<T, T1>()
+            {
+                _typeMap[typeof (T)] = typeof (T1);
+            }
 
-			VerifyCreations(db);
-		}
+            public bool CanResolve(Type type)
+            {
+                return _typeMap.ContainsKey(type) || IsClass(type);
+            }
 
+            private bool IsClass(Type type)
+            {
+                return type.GetTypeInfo().IsClass;
+            }
 
-		[Test]
-		[Category("IoC.TinyIoC")]
-		public void BulkInsertAndSelect()
-		{
-			var db = new TestDb(false, new ContractResolver(t => _container.CanResolve(t), (t, op) => _container.Resolve(t)));
+            public T Resolve<T>()
+            {
+                return (T) Resolve(typeof (T));
+            }
 
-			db.CreateTable<IProduct>();
-			db.CreateTable<IOrder>();
-			db.CreateTable<IOrderLine>();
-			db.CreateTable<IOrderHistory>();
+            public object Resolve(Type type)
+            {
+                if (_typeMap.ContainsKey(type))
+                {
+                    var mappedType = _typeMap[type];
+                    return Activator.CreateInstance(mappedType);
+                }
+                if (IsClass(type))
+                {
+                    return Activator.CreateInstance(type);
+                }
+                throw new NotSupportedException();
+            }
+        }
 
-			var rnd = new Random();
-			var data = new List<IOrderLine>();
-			for (int i = 0; i < 100; i++)
-			{
-				var l = _container.Resolve<IOrderLine>();
+        [Test]
+        [Category("IoC.TinyIoC")]
+        public void BulkInsertAndSelect()
+        {
+            var db = new TestDb(false, new ContractResolver(t => _container.CanResolve(t), (t, op) => _container.Resolve(t)));
 
-				l.OrderId = rnd.Next(1, 100);
-				l.ProductId = rnd.Next(1, 100);
-				l.Quantity = rnd.Next(1, 25);
-				l.Status = OrderLineStatus.Shipped;
-				l.UnitPrice = rnd.Next(0, 100);
+            db.CreateTable<IProduct>();
+            db.CreateTable<IOrder>();
+            db.CreateTable<IOrderLine>();
+            db.CreateTable<IOrderHistory>();
 
-				data.Add(l);
-			}
+            var rnd = new Random();
+            var data = new List<IOrderLine>();
+            for (var i = 0; i < 100; i++)
+            {
+                var l = _container.Resolve<IOrderLine>();
 
-			db.InsertAll(data.ToArray());
+                l.OrderId = rnd.Next(1, 100);
+                l.ProductId = rnd.Next(1, 100);
+                l.Quantity = rnd.Next(1, 25);
+                l.Status = OrderLineStatus.Shipped;
+                l.UnitPrice = rnd.Next(0, 100);
 
-			var results = db.Table<IOrderLine>();
+                data.Add(l);
+            }
 
-			Assert.AreEqual(data.Count, results.Count());
-			Assert.AreEqual(data.First().UnitPrice, results.First().UnitPrice);
-			Assert.AreEqual(data.Last().UnitPrice, results.Last().UnitPrice);
-		}
-	}
+            db.InsertAll(data.ToArray());
+
+            var results = db.Table<IOrderLine>();
+
+            Assert.AreEqual(data.Count, results.Count());
+            Assert.AreEqual(data.First().UnitPrice, results.First().UnitPrice);
+            Assert.AreEqual(data.Last().UnitPrice, results.Last().UnitPrice);
+        }
+
+        [Test]
+        public void CreateThemByInterface()
+        {
+            var db = new TestDb();
+
+            db.CreateTable<IProduct>();
+            db.CreateTable<IOrder>();
+            db.CreateTable<IOrderLine>();
+            db.CreateTable<IOrderHistory>();
+
+            VerifyCreations(db);
+        }
+    }
 }
