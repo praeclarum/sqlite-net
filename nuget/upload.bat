23ffc68dd35238a274da3e8e56e986103161bb5b
Øystein Krog <oystein.krog@gmail.com>
2015-03-29 19:19:43 +0200

    version: 3.0.1

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,2 +1,2 @@
-..\.nuget\nuget push output\SQLite.Net-PCL.2.5.1.nupkg
-..\.nuget\nuget push output\SQLite.Net.Async-PCL.2.5.1.nupkg
+..\.nuget\nuget push output\SQLite.Net-PCL.3.0.1-beta.nupkg
+..\.nuget\nuget push output\SQLite.Net.Async-PCL.3.0.1-beta.nupkg
