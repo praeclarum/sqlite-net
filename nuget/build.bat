27ec7616b7793464bf75eec5d46c3591e8e23e6e
Matthew Leibowitz <mattleibow@live.com>
2015-08-24 21:48:43 +0200

    Split SQLite.Net so that other platforms can be created.  - SQLite.Net still functions as before, but the SQLite.Net.dll was moved out  - SQLite.Net.Core now contains the SQLite.Net.dll  - SQLite.Net.Async now depends on SQLite.Net.Core

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,3 +1,4 @@
 @mkdir output
+..\.nuget\nuget pack SQLite.Net.Core.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.nuspec -o output
 ..\.nuget\nuget pack SQLite.Net.Async.nuspec -o output
--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,2 +1,3 @@
+..\.nuget\nuget push output\SQLite.Net.Core-PCL.3.0.5.nupkg
 ..\.nuget\nuget push output\SQLite.Net-PCL.3.0.5.nupkg
 ..\.nuget\nuget push output\SQLite.Net.Async-PCL.3.0.5.nupkg
