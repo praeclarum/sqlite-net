0de70e278832c49cb0f170709e3ae5109ec70011
Øystein Krog <oystein.krog@gmail.com>
2015-11-06 17:19:36 +0100

    version: Bump to 3.1.1

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,3 +1,3 @@
-..\.nuget\nuget push output\SQLite.Net.Core-PCL.3.1.0.nupkg
-..\.nuget\nuget push output\SQLite.Net-PCL.3.1.0.nupkg
-..\.nuget\nuget push output\SQLite.Net.Async-PCL.3.1.0.nupkg
+..\.nuget\nuget push output\SQLite.Net.Core-PCL.3.1.1.nupkg
+..\.nuget\nuget push output\SQLite.Net-PCL.3.1.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Async-PCL.3.1.1.nupkg
