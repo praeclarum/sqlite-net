e7b8303677a3a0190cc601c091a139b60b464bc1
Frank A. Krueger <fak@praeclarum.org>
2016-01-11 17:56:23 -0800

    Update to .raw 0.8.6

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 
 
 
-all: test	
+all: test nuget
 
 test: tests/bin/Debug/SQLite.Tests.dll
 	nunit-console tests/bin/Debug/SQLite.Tests.dll
 
 tests/bin/Debug/SQLite.Tests.dll:
 	xbuild tests/SQLite.Tests.csproj
+
+nuget: srcnuget pclnuget
+
+srcnuget: src/SQLite.cs src/SQLiteAsync.cs sqlite-net.nuspec
+	nuget pack sqlite-net.nuspec
+
+pclnuget: src/SQLite.cs src/SQLiteAsync.cs
+	xbuild /p:Configuration=Release nuget/SQLite-net/SQLite-net.sln
+	nuget pack sqlite-net-pcl.nuspec -o .\
 
--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Krueger Systems, Inc.")]
 [assembly: AssemblyProduct("SQLite-net")]
-[assembly: AssemblyCopyright("Copyright © 2009-2014 Krueger Systems, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2009-2016 Krueger Systems, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
