77662f8cd413ea782ee01c2aa99dc67816bad260
Ali Hosseini Khayat <alkhayat@microsoft.com>
2016-04-28 00:55:06 -0700

    Adding support for using WinSQLite.dll from WinRT which is built into Windows 10.10586.0 and above.

--- src/SQLite.Net.Platform.WinRT/SQLitePlatformWinRT.cs
+++ src/SQLite.Net.Platform.WinRT/SQLitePlatformWinRT.cs
@@ -5,9 +5,14 @@ namespace SQLite.Net.Platform.WinRT
 {
     public class SQLitePlatformWinRT : ISQLitePlatform
     {
-        public SQLitePlatformWinRT(string tempFolderPath = null)
+        /// <summary>
+        /// Creates a SQLite platform object for use from WinRT.
+        /// </summary>
+        /// <param name="tempFolderPath">Optional: Temporary folder path. Defaults to <see cref="Windows.Storage.ApplicationData.Current.TemporaryFolder.Path"/></param>
+        /// <param name="useWinSqlite">Optional: Whether to use WinSQLite instead of SQLite. WinSQLite is built-in to Windows 10.0.10586.0 and above. Using it can reduce app size and potentially increase SQLite load time.</param>
+        public SQLitePlatformWinRT(string tempFolderPath = null, bool useWinSqlite = false)
         {
-            SQLiteApi = new SQLiteApiWinRT(tempFolderPath);
+            SQLiteApi = new SQLiteApiWinRT(tempFolderPath, useWinSqlite);
             VolatileService = new VolatileServiceWinRT();
             StopwatchFactory = new StopwatchFactoryWinRT();
             ReflectionService = new ReflectionServiceWinRT();
