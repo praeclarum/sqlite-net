665007aa5d70f018d14d44e120e05dae5b339e41
Andrew Tavera <andrewtavera@outlook.com>
2017-07-11 03:13:17 -0400

    StartsWith, EndsWith, and Contains generated SQL behaves like the System.String methods used to construct the query: Contains is case sensitive, using instr() instead of like(). ToUpper() or ToLower can be used to compare without case sensitivity. StartsWith and EndsWith is case sensitive unless a case insensitive StringComparison argument is passed using substr() = ? or the original like() depending upon case sensitivity

--- tests/StringQueryTest.cs
+++ tests/StringQueryTest.cs
@@ -62,8 +62,16 @@ namespace SQLite.Tests
 		{
 			var fs = db.Table<Product> ().Where (x => x.Name.StartsWith ("F")).ToList ();
 			Assert.AreEqual (2, fs.Count);
-			
-			var bs = db.Table<Product> ().Where (x => x.Name.StartsWith ("B")).ToList ();
+		    
+		    var lfs = db.Table<Product>().Where(x => x.Name.StartsWith("f")).ToList();
+		    Assert.AreEqual(0, lfs.Count);
+
+
+		    var lfs2 = db.Table<Product>().Where(x => x.Name.StartsWith("f",StringComparison.OrdinalIgnoreCase)).ToList();
+		    Assert.AreEqual(2, lfs2.Count);
+
+
+            var bs = db.Table<Product> ().Where (x => x.Name.StartsWith ("B")).ToList ();
 			Assert.AreEqual (1, bs.Count);
 		}
 		
@@ -71,20 +79,32 @@ namespace SQLite.Tests
 		public void EndsWith ()
 		{
 			var fs = db.Table<Product> ().Where (x => x.Name.EndsWith ("ar")).ToList ();
-			Assert.AreEqual (2, fs.Count);
-			
-			var bs = db.Table<Product> ().Where (x => x.Name.EndsWith ("o")).ToList ();
+		    Assert.AreEqual (2, fs.Count);
+
+		    var lfs = db.Table<Product>().Where(x => x.Name.EndsWith("Ar")).ToList();
+		    Assert.AreEqual(0, lfs.Count);
+		    
+            var bs = db.Table<Product> ().Where (x => x.Name.EndsWith ("o")).ToList ();
 			Assert.AreEqual (1, bs.Count);
 		}
 		
 		[Test]
 		public void Contains ()
 		{
-			var fs = db.Table<Product> ().Where (x => x.Name.Contains ("o")).ToList ();
-			Assert.AreEqual (2, fs.Count);
-			
-			var bs = db.Table<Product> ().Where (x => x.Name.Contains ("a")).ToList ();
+		    var fs = db.Table<Product>().Where(x => x.Name.Contains("o")).ToList();
+		    Assert.AreEqual(2, fs.Count);
+		    
+            var lfs = db.Table<Product> ().Where (x => x.Name.Contains ("O")).ToList ();
+			Assert.AreEqual (0, lfs.Count);
+
+		    var lfsu = db.Table<Product>().Where(x => x.Name.ToUpper().Contains("O")).ToList();
+		    Assert.AreEqual(2, lfsu.Count);
+
+            var bs = db.Table<Product> ().Where (x => x.Name.Contains ("a")).ToList ();
 			Assert.AreEqual (2, bs.Count);
-		}
+
+		    var zs = db.Table<Product>().Where(x => x.Name.Contains("z")).ToList();
+		    Assert.AreEqual(0, zs.Count);
+        }
 	}
 }
