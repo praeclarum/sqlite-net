9209ea9e0585e05f223e9e62d8bfcbbfab6754d4
Frank A. Krueger <fak@praeclarum.org>
2017-08-02 18:03:36 -0700

    Improve the doc comments for CreateFlags
    Trying to avoid confusion as in #421

--- src/SQLite.cs
+++ src/SQLite.cs
@@ -128,22 +128,28 @@ namespace SQLite
 	[Flags]
 	public enum CreateFlags
 	{
+		/// <summary>
+		/// Use the default creation options
+		/// </summary>
 		None = 0x000,
 		/// <summary>
-		/// Create a primary key for properties called 'Id'
+		/// Create a primary key index for a property called 'Id' (case-insensitive).
+		/// This avoids the need for the [PrimaryKey] attribute.
 		/// </summary>
 		ImplicitPK = 0x001,
 		/// <summary>
-		/// Create an index for properties ending in 'Id'
+		/// Create indices for properties ending in 'Id' (case-insensitive).
 		/// </summary>
 		ImplicitIndex = 0x002,
 		/// <summary>
-		/// Create a primary key for properties called 'Id' and
-		/// create an index for properties ending in 'Id'.
+		/// Create a primary key for a property called 'Id' and
+		/// create an indices for properties ending in 'Id' (case-insensitive).
 		/// </summary>
 		AllImplicit = 0x003,
 		/// <summary>
-		/// Force the primary key to be auto incrementing
+		/// Force the primary key property to be auto incrementing.
+		/// This avoids the need for the [AutoIncrement] attribute.
+		/// The primary key property on the class should have type int or long.
 		/// </summary>
 		AutoIncPK = 0x004,
 		/// <summary>
