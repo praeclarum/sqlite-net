bb5eee35fbc666a3afdc67611aaffb5ebdd4eb3e
Frank A. Krueger <fak@praeclarum.org>
2017-08-12 18:34:37 -0700

    Test enums defined as bytes
    Fixes #33

--- tests/EnumTest.cs
+++ tests/EnumTest.cs
@@ -70,6 +70,7 @@ namespace SQLite.Tests
             {
                 CreateTable<TestObj>();
 				CreateTable<StringTestObj>();
+				CreateTable<ByteTestObj> ();
             }
         }
 
@@ -120,5 +121,50 @@ namespace SQLite.Tests
 
 			db.Close();
 		}
-    }
+
+		public enum ByteTestEnum : byte
+		{
+			Value1 = 1,
+
+			Value2 = 2,
+
+			Value3 = 3
+		}
+
+		public class ByteTestObj
+		{
+			[PrimaryKey]
+			public int Id { get; set; }
+			public ByteTestEnum Value { get; set; }
+
+			public override string ToString ()
+			{
+				return string.Format ("[ByteTestObj: Id={0}, Value={1}]", Id, Value);
+			}
+		}
+
+		[Test]
+		public void Issue33_ShouldPersistAndReadByteEnum ()
+		{
+			var db = new TestDb (TestPath.GetTempFileName ());
+
+			var obj1 = new ByteTestObj () { Id = 1, Value = ByteTestEnum.Value2 };
+			var obj2 = new ByteTestObj () { Id = 2, Value = ByteTestEnum.Value3 };
+
+			var numIn1 = db.Insert (obj1);
+			var numIn2 = db.Insert (obj2);
+			Assert.AreEqual (1, numIn1);
+			Assert.AreEqual (1, numIn2);
+
+			var result = db.Query<ByteTestObj> ("select * from ByteTestObj order by Id").ToList ();
+			Assert.AreEqual (2, result.Count);
+			Assert.AreEqual (obj1.Value, result[0].Value);
+			Assert.AreEqual (obj2.Value, result[1].Value);
+
+			Assert.AreEqual (obj1.Id, result[0].Id);
+			Assert.AreEqual (obj2.Id, result[1].Id);
+
+			db.Close ();
+		}
+	}
 }
