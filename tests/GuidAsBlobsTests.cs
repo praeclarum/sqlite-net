1a2b97c523346c85eba7baf3771e953e604579d3
alexander.marek <alexander.marek@opti-q.com>
2018-08-08 15:12:24 +0200

    Added async tests for option StoreGuidsAsBlobs

--- tests/GuidAsBlobsTests.cs
+++ tests/GuidAsBlobsTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 #if NETFX_CORE
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using SetUp = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.TestInitializeAttribute;
@@ -99,6 +99,53 @@ namespace SQLite.Tests {
 		    Assert.AreEqual (obj2.Id, result[0].Id);
 
 		    db.Close ();
+	    }
+
+
+	    [Test]
+	    public async Task ShouldQueryGuidCorrectlyAsync () {
+
+			var db = new SQLiteAsyncConnection (TestPath.GetTempFileName (), storeGuidsAsBlobs: true);
+		    await db.CreateTableAsync<TestObj> (CreateFlags.AutoIncPK);
+
+			var obj1 = new TestObj () { Id = new Guid ("36473164-C9E4-4CDF-B266-A0B287C85623"), Text = "First Guid Object" };
+		    var obj2 = new TestObj () { Id = new Guid ("BC5C4C4A-CA57-4B61-8B53-9FD4673528B6"), Text = "Second Guid Object" };
+
+		    var numIn1 = await db.InsertAsync (obj1);
+		    var numIn2 = await db.InsertAsync (obj2);
+		    Assert.AreEqual (1, numIn1);
+		    Assert.AreEqual (1, numIn2);
+
+		    var result = (await db.QueryAsync<TestObj> ("select * from TestObj where id = ?", obj2.Id)).ToList ();
+		    Assert.AreEqual (1, result.Count);
+		    Assert.AreEqual (obj2.Text, result[0].Text);
+
+		    Assert.AreEqual (obj2.Id, result[0].Id);
+
+		    await db.CloseAsync ();
+	    }
+
+	    [Test]
+	    public async Task ShouldQueryGuidCorrectlyUsingLinqAsync () {
+
+			var db = new SQLiteAsyncConnection (TestPath.GetTempFileName (), storeGuidsAsBlobs: true);
+		    await db.CreateTableAsync<TestObj> (CreateFlags.AutoIncPK);
+
+			var obj1 = new TestObj () { Id = new Guid ("36473164-C9E4-4CDF-B266-A0B287C85623"), Text = "First Guid Object" };
+		    var obj2 = new TestObj () { Id = new Guid ("BC5C4C4A-CA57-4B61-8B53-9FD4673528B6"), Text = "Second Guid Object" };
+
+		    var numIn1 = await db.InsertAsync (obj1);
+		    var numIn2 = await db.InsertAsync (obj2);
+		    Assert.AreEqual (1, numIn1);
+		    Assert.AreEqual (1, numIn2);
+
+		    var result = await db.Table<TestObj>().Where (to => to.Id == obj2.Id).ToListAsync ();
+		    Assert.AreEqual (1, result.Count);
+		    Assert.AreEqual (obj2.Text, result[0].Text);
+
+		    Assert.AreEqual (obj2.Id, result[0].Id);
+
+		    await db.CloseAsync ();
 	    }
 
 		[Test]
