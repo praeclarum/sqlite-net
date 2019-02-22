a679da1a1916a77c85765a8a9fcfb6cfbd6f68f3
kinderry <781535766@qq.com>
2019-02-22 23:17:09 +0800

    fix #811 add SingleQuery method and Tests add StringQuery IsNullOrEmpty

--- tests/StringQueryTest.cs
+++ tests/StringQueryTest.cs
@@ -17,22 +17,24 @@ namespace SQLite.Tests
 	public class StringQueryTest
 	{
 		TestDb db;
-		
+
 		[SetUp]
 		public void SetUp ()
 		{
 			db = new TestDb ();
 			db.CreateTable<Product> ();
-			
+
 			var prods = new[] {
 				new Product { Name = "Foo" },
 				new Product { Name = "Bar" },
 				new Product { Name = "Foobar" },
+				new Product { Name = null, Price=100 },
+				new Product { Name = string.Empty,Price=1000 },
 			};
-			
+
 			db.InsertAll (prods);
 		}
-		
+
 		[Test]
 		public void StringEquals ()
 		{
@@ -62,49 +64,59 @@ namespace SQLite.Tests
 		{
 			var fs = db.Table<Product> ().Where (x => x.Name.StartsWith ("F")).ToList ();
 			Assert.AreEqual (2, fs.Count);
-		    
-		    var lfs = db.Table<Product>().Where(x => x.Name.StartsWith("f")).ToList();
-		    Assert.AreEqual(0, lfs.Count);
+
+			var lfs = db.Table<Product>().Where(x => x.Name.StartsWith("f")).ToList();
+			Assert.AreEqual(0, lfs.Count);
 
 
-		    var lfs2 = db.Table<Product>().Where(x => x.Name.StartsWith("f",StringComparison.OrdinalIgnoreCase)).ToList();
-		    Assert.AreEqual(2, lfs2.Count);
+			var lfs2 = db.Table<Product>().Where(x => x.Name.StartsWith("f",StringComparison.OrdinalIgnoreCase)).ToList();
+			Assert.AreEqual(2, lfs2.Count);
 
 
-            var bs = db.Table<Product> ().Where (x => x.Name.StartsWith ("B")).ToList ();
+			var bs = db.Table<Product> ().Where (x => x.Name.StartsWith ("B")).ToList ();
 			Assert.AreEqual (1, bs.Count);
 		}
-		
+
 		[Test]
 		public void EndsWith ()
 		{
 			var fs = db.Table<Product> ().Where (x => x.Name.EndsWith ("ar")).ToList ();
-		    Assert.AreEqual (2, fs.Count);
+			Assert.AreEqual (2, fs.Count);
 
-		    var lfs = db.Table<Product>().Where(x => x.Name.EndsWith("Ar")).ToList();
-		    Assert.AreEqual(0, lfs.Count);
-		    
-            var bs = db.Table<Product> ().Where (x => x.Name.EndsWith ("o")).ToList ();
+			var lfs = db.Table<Product>().Where(x => x.Name.EndsWith("Ar")).ToList();
+			Assert.AreEqual(0, lfs.Count);
+
+			var bs = db.Table<Product> ().Where (x => x.Name.EndsWith ("o")).ToList ();
 			Assert.AreEqual (1, bs.Count);
 		}
-		
+
 		[Test]
 		public void Contains ()
 		{
-		    var fs = db.Table<Product>().Where(x => x.Name.Contains("o")).ToList();
-		    Assert.AreEqual(2, fs.Count);
-		    
-            var lfs = db.Table<Product> ().Where (x => x.Name.Contains ("O")).ToList ();
+			var fs = db.Table<Product>().Where(x => x.Name.Contains("o")).ToList();
+			Assert.AreEqual(2, fs.Count);
+
+			var lfs = db.Table<Product> ().Where (x => x.Name.Contains ("O")).ToList ();
 			Assert.AreEqual (0, lfs.Count);
 
-		    var lfsu = db.Table<Product>().Where(x => x.Name.ToUpper().Contains("O")).ToList();
-		    Assert.AreEqual(2, lfsu.Count);
+			var lfsu = db.Table<Product>().Where(x => x.Name.ToUpper().Contains("O")).ToList();
+			Assert.AreEqual(2, lfsu.Count);
 
-            var bs = db.Table<Product> ().Where (x => x.Name.Contains ("a")).ToList ();
+			var bs = db.Table<Product> ().Where (x => x.Name.Contains ("a")).ToList ();
 			Assert.AreEqual (2, bs.Count);
 
-		    var zs = db.Table<Product>().Where(x => x.Name.Contains("z")).ToList();
-		    Assert.AreEqual(0, zs.Count);
-        }
+			var zs = db.Table<Product>().Where(x => x.Name.Contains("z")).ToList();
+			Assert.AreEqual(0, zs.Count);
+		}
+		[Test]
+		public void IsNullOrEmpty ()
+		{
+			var isnullorempty = db.Table<Product>().Where(x => string.IsNullOrEmpty(x.Name)).ToList();
+			Assert.AreEqual (2, isnullorempty.Count);
+
+			var isnotnullorempty = db.Table<Product>().Where(x => !string.IsNullOrEmpty(x.Name)).ToList();
+			Assert.AreEqual(3, isnotnullorempty.Count);
+
+		}
 	}
 }
