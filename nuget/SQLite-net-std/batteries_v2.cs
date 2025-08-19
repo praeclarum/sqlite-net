4fa56be365deadbf622d0450f157a6e349d3b72a
Eric Sink <eric@sourcegear.com>
2025-08-19 12:02:37 -0500

    update SQLitePCLRaw dependency to 3.0.1.  since bundle_green has been deprecated, implement the same approach here in SQLite-net-pcl, using the system SQLite library on iOS and e_sqlite3 everywhere else.

---
+++ nuget/SQLite-net-std/batteries_v2.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace SQLitePCL
+{
+    internal static class Batteries_V2
+    {
+	    public static void Init()
+	    {
+#if PROVIDER_sqlite3
+		    SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
+#elif PROVIDER_e_sqlite3
+		    SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
+#else
+#error batteries_v2.cs built with nothing specified
+#endif
+	    }
+    }
+}
+
